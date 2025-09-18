3dd8df3c9623056d6f1138f1de5d914bd2fe4ecd
jelisavac-l <jelisavacluka63@gmail.com>
2025-09-18 22:13:38 +0200

    feat: footer

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <div>
     <NavBar />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script setup>
+import Footer from './components/Footer.vue';
 import NavBar from './components/NavBar.vue'
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+
+    <footer class="bg-ppp-primary text-white flex align-middle justify-center p-4">
+        <p class="pe-1 font-semibold">Pesmarica++ &copy;</p>
+        <a href="https://github.com/jelisavac-l" class="hover:text-ppp-accent">
+            Luka Jelisavac 2022/0554
+        </a>
+    </footer>
+
+</template>
+
+<script setup>
+
+</script>

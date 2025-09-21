eb73f8ea2d1ac3edfd0fd13f9ccd10f17f822208
jelisavac-l <jelisavacluka63@gmail.com>
2025-09-21 23:21:29 +0200

    fix: local storage fix

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,33 @@
     <main class="flex-grow">
       <router-view />
     </main>
-    
     <Footer />
   </div>
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import Footer from './components/Footer.vue';
 import NavBar from './components/NavBar.vue'
+import api from './assets/api';
+import { useUserStore } from './stores/user';
+
+const userStore = useUserStore()
+
+const logout = () => {
+  userStore.logout()
+}
+
+const checkAuth = async () => {
+  try{
+    const response = await api.post('/util/ping');
+  } catch {
+    logout();
+  }
+}
+
+onMounted(()=> {
+  checkAuth()
+})
+
 </script>
